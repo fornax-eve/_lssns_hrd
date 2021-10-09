e1dfd9b018b0e09ab27b5b0f018dfcce5b35f843
An Ki <kiyko.andriy@gmail.com>
2021-10-09 13:19:44 +0200

    days of the week

---
+++ script_hard.js
@@ -0,0 +1,19 @@
+'use script'
+
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let date = new Date();
+console.log(week[date.getDay()])
+
+for( let day of week) {
+    let nday = day;
+    if (day == week[date.getDay()]) {
+        nday = day.bold();
+    }
+    if (day == 'Sunday' || day == 'Saturday') {
+        nday = nday.italics();
+    }
+    let newEl = document.createElement('p');
+    newEl.innerHTML = nday;
+    document.body.appendChild(newEl);
+    console.log(nday)
+}
