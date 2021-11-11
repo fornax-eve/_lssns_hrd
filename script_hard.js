8ed52abe67aa0620f804024ae3c49ecca03c526a
An Ki <kiyko.andriy@gmail.com>
2021-11-11 18:25:13 +0100

    redaction

--- script_hard.js
+++ script_hard.js
@@ -1,33 +1,41 @@
 let ru_week = ['Понедельник', 'Вторник', 'Среда', 'Четвер', 'Пятница', 'Субота', 'Воскресенье'];
 let en_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let sposob = prompt('Введите if, switch или array для выбора способа решения задачи')
 let lang = prompt('Введите en или ru')
 
-
-if (lang == 'ru') {
-    ru_week.forEach(a => console.log(a))
-} else if (lang == 'en') {
-    en_week.forEach(b => console.log(b))
+if (sposob == 'if') {
+    if (lang == 'ru') {
+        ru_week.forEach(a => console.log(a))
+    } else if (lang == 'en') {
+        en_week.forEach(b => console.log(b))
+    } else {
+        console.log("Некорректное введение")
+    }
+    ;
+} else if (sposob == 'switch') {
+    switch (lang) {
+        case "en":
+            en_week.forEach(b => console.log(b));
+            break;
+        case "ru":
+            ru_week.forEach(a => console.log(a));
+            break;
+        default:
+            console.log('Некорректный выбор языка')
+    }
+} else if (sposob == 'array') {
+    let week = {
+        ru: ru_week,
+        en: en_week
+    }
+    week[lang].forEach(b => console.log(b))
 } else {
-    console.log("Некорректное введение")
-};
+    alert('Неправильный ввод данных!')
+}
 
-switch (lang) {
-    case "en":
-        en_week.forEach(b => console.log(b));
-        break;
-    case "ru":
-        ru_week.forEach(a => console.log(a));
-        break;
-    default:
-        console.log('Некорректный выбор языка')
-};
+setTimeout(() => {
+    let namePerson = prompt('Введите Артем, Александр или любое другое имя');
+    (namePerson == 'Артем') ? console.log('Директор') : (namePerson == 'Александр') ? console.log('Преподаватель') :
+        console.log('Студент')
+}, 5000)
 
-let week = {ru : ru_week,
-    en : en_week}
-let massive = week[lang].forEach(b => console.log(b))
-console.log(massive)
-
-
-let namePerson = prompt('Введите Артем, Александр или любое другое имя');
-(namePerson == 'Артем') ? console.log('Директор') : (namePerson == 'Александр') ? console.log('Преподаватель') :
-    console.log('Студент')
